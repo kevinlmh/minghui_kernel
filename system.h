2295fe9a5a2ddf84b62d1317277d8a9b666f295f
Minghui Liu <minghui.liu@trincoll.edu>
2015-01-31 16:27:33 -0500

    Fix function prototypes fixed parameter type conflicts with system.c

--- system.h
+++ system.h
@@ -2,11 +2,11 @@
 #define __SYSTEM_H
 
 /* system.c */
-unsigned char *memcpy(unsigned char *dest, const unsigned char *src, int count);
-unsigned char *memset(unsigned char *dest, unsigned char val, int count);
-unsigned short *memsetw(unsigned short *dest, unsigned short val, int count);
-int strlen(const char *str);
-unsigned char inportb (unsigned short _port);
-void outportb (unsigned short _port, unsigned char _data);
+void *memcpy(void *dest, const void *src, size_t count);
+void *memset(void *dest, int8_t val, size_t count);
+uint16_t *memsetw(uint16_t *dest, uint16_t val, size_t count);
+size_t strlen(const int8_t *str);
+uint8_t inportb(uint16_t _port);
+void outportb (uint16_t _port, uint8_t _data);
 
 #endif
